43f78e15c58ddf285b0aebd39206bc4e2f3d91d6
agungxpratama <agungxpratama@gmail.com>
2020-01-16 11:11:57 +0700

    update tim dan widget keterangan

---
+++ application/views/tim/dashboard.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <?php $this->load->view("admin/_patrials/head.php") ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <!-- /.navbar -->
+   <?php $this->load->view("admin/_patrials/navbar.php") ?>
+ 
+  <!-- Sidebar -->
+   <?php $this->load->view("admin/_patrials/admin_sidebar.php") ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <?php $this->load->view("admin/_patrials/admin_sidebar.php") ?>
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.2-pre
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<?php $this->load->view("admin/_patrials/js.php") ?>
+</body>
+</html>
